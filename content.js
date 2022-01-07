5a64e7a11fd230a2f44e2e015b0cddd6c7e9f283
Samy HENDEL <hendels@w.ensma.fr>
2022-01-07 17:59:36 +0100

    chrome extension version 2

--- content.js
+++ content.js
@@ -1,13 +1,10 @@
-// Grab the Execute Query Button from "https://dbpedia.org/sparql"
-const executeQuery = document.querySelector("#run")
-
 // Create a div element to contain label and input
 const div = document.createElement("div")
 
 // Creat the label element
 const label = document.createElement("label")
 label.setAttribute("for", "algo")
-label.append("Do you want to use Luis algorithms ?")
+label.append("Do you want to use Louise algorithms ?")
 label.style.marginLeft = "10px"
 
 // Create the checkbox element
@@ -19,8 +16,11 @@ input.setAttribute("name", "algo")
 // Apprnd the two elements to the div element
 div.append(input, label)
 
+// Grab the Execute Query Button from "https://dbpedia.org/sparql"
+const executeQuery = document.querySelector("#run")
+
 // And then append the div element right before the execute query button
-document.querySelector("#run").insertAdjacentElement("beforeBegin", div)
+executeQuery.insertAdjacentElement("beforeBegin", div)
 
 // Create a global variable to hold true or fale in order to know whether a user has thecked the checkbox or not
 let luisAlgorithmsChecked = false
@@ -40,10 +40,49 @@ input.addEventListener('change', () => {
     }
 })
 
-const luisAlgorithm =  (e) => {
+// JSON PLACEHOLDER API
+const BASE_URL = "https://jsonplaceholder.typicode.com/users";
+let users = [];
+
+// ON LOAD
+window.addEventListener("load", async() => {
+    users = await loadUsers(BASE_URL);
+} )
+
+
+const luisAlgorithm = async (e) => {
     e.preventDefault()
 
-     alert("I am going to use Luis algorithms")
+    console.log(users)
 }
 
+// const userDiv = document.createElement('div')
+// userDiv.id = "users"
+// const ol = document.createElement("ol")
+// userDiv.append(ol)
+// const li = document.createElement("li")
+// li.setAttribute("v-for", "user in users")
+// li.append("{{user.name}}")
+// ol.append(li)
+// document.querySelector("#options").insertAdjacentElement("beforebegin", userDiv)
 
+// loadUsers from JSONPLACEHOLDER API
+const loadUsers = async(url) => {
+    const response = await fetch(url)
+    const users = await response.json()
+    return users
+} 
+
+// Create Vue component
+Vue.component("user", {
+    template: "<p>{{users.name}}</p>"
+})
+
+// WASM SECTION : LOAD WASM : hello.wasm file
+const go = new Go();
+
+WebAssembly.instantiateStreaming(fetch('./wasm/hello.wasm'), go.importObject).then(
+	(result) => {
+		go.run(result.instance);
+	}
+);
